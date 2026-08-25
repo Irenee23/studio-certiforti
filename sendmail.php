297e110c3365087396bc6d6ec70f23cfefae25b0
Irenee23 <examaxson@gmail.com>
2026-08-25 18:41:30 +0200

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,16 +1,35 @@
-$mail->Username   = 'studio.certiforti@gmail.com';
-$mail->Password   = 'vvwxeoairjuqbjxf';
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'tls';
 $mail->SMTPAuth = true;
+$mail->Username = 'studio.certiforti@gmail.com';
+$mail->Password = 'vvwxeoairjuqbjxf'; // password per app
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
 $mail->setFrom('studio.certiforti@gmail.com', 'Studio Certiforti');
-$mail->addAddress('tuoindirizzo@dominio.it'); // destinatario di test
+$mail->addAddress('studio.certiforti@gmail.com'); // destinatario test
+
 $mail->Subject = 'Test invio email';
 $mail->Body = 'Email di prova inviata da PHP tramite Gmail.';
+
 if(!$mail->send()) {
     echo 'Errore nell\'invio: ' . $mail->ErrorInfo;
 } else {
     echo 'Email inviata con successo!';
 }
-
