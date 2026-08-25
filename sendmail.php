f10eb007ebd6ddef1ad1f8e1790417b8c317094f
Irenee23 <examaxson@gmail.com>
2026-08-25 18:22:22 +0200

    Configure email settings for Gmail SMTP

--- sendmail.php
+++ sendmail.php
@@ -1,3 +1,16 @@
 $mail->Username   = 'studio.certiforti@gmail.com';
 $mail->Password   = 'vvwx eoai rjuq bjxf';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->setFrom('studio.certiforti@gmail.com', 'Studio Certiforti');
+$mail->addAddress('tuoindirizzo@dominio.it'); // destinatario di test
+$mail->Subject = 'Test invio email';
+$mail->Body = 'Email di prova inviata da PHP tramite Gmail.';
+if(!$mail->send()) {
+    echo 'Errore nell\'invio: ' . $mail->ErrorInfo;
+} else {
+    echo 'Email inviata con successo!';
+}
 
